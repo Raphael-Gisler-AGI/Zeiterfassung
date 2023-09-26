49c6dd8ff66a610c4153c94be9156edbd849e59a
Raphael Gisler <raphael.gisler@agilita.ch>
2023-09-26 11:26:38 +0200

    Display the create button

--- webapp/controller/Time.controller.js
+++ webapp/controller/Time.controller.js
@@ -1,10 +1,29 @@
-sap.ui.define([
-	"./BaseController"
-], function(
-	Controller
-) {
-	"use strict";
+sap.ui.define(
+  ["./BaseController", "sap/ui/core/date/UI5Date"],
+  function (Controller, UI5Date) {
+    "use strict";
 
-	return Controller.extend("sap.ui.agi.zeiterfassung.controller.Time", {
-	});
-});
+    return Controller.extend("sap.ui.agi.zeiterfassung.controller.Time", {
+      handleAppointmentCreate: function () {
+        this._createInitialDialogValues(
+          this.getView().byId("SPC1").getStartDate()
+        );
+      },
+      _createInitialDialogValues: function (oDate) {
+		console.log(oDate)
+        this._arrangeDialogFragment("Create appointment");
+      },
+      _arrangeDialogFragment: function (text) {
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: "sap.ui.agi.zeiterfassung.view.Create",
+          });
+        }
+
+        this.pDialog.then(function (oDialog) {
+          oDialog.open();
+        });
+      }
+    });
+  }
+);
