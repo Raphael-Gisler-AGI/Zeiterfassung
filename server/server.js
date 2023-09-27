058c15a8dc20eadb392057b41ecb3e876e72d48d
Raphael Gisler <raphael.gisler@agilita.ch>
2023-09-27 11:21:09 +0200

    Parse Data before pushing to data file

--- server/server.js
+++ server/server.js
@@ -5,12 +5,19 @@ const fs = require("fs");
 const fileName = "./data/data.json";
 const file = require(fileName);
 
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "http://localhost:8080",
+  })
+);
+
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
 app.get("/createEntry", (req, res) => {
-  const entry = req.query.entry;
+  const entry = JSON.parse(req.query.entry);
   file.User.Entries.push(entry);
   fs.writeFile(fileName, JSON.stringify(file), (err) => {
     if (err) res.sendStatus(400);
@@ -19,6 +26,5 @@ app.get("/createEntry", (req, res) => {
 });
 
 app.get("/getEntries", (req, res) => {
-  console.log(file.User.Entries)
-  res.send(file.User.Entries)
-})
+  res.send(file.User.Entries);
+});
