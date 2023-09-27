1dae7aa992dd80a1a6e4072255f78ff3ff69e006
Raphael Gisler <raphael.gisler@agilita.ch>
2023-09-27 08:00:30 +0200

    Implement Create and Get
    This allows the webapp to get and create data

--- server/server.js
+++ server/server.js
@@ -9,3 +9,16 @@ app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
+app.get("/createEntry", (req, res) => {
+  const entry = req.query.entry;
+  file.User.Entries.push(entry);
+  fs.writeFile(fileName, JSON.stringify(file), (err) => {
+    if (err) res.sendStatus(400);
+  });
+  res.sendStatus(200);
+});
+
+app.get("/getEntries", (req, res) => {
+  console.log(file.User.Entries)
+  res.send(file.User.Entries)
+})
